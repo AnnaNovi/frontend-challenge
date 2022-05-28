6906e869c590328f187dd8107be83f033d4c221b
Anna <annprogram-gh@mail.ru>
2022-05-29 00:57:20 +0300

    create app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Все котики</router-link>
+    <router-link to="/favorite">Любимые котики</router-link>
   </nav>
   <router-view />
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-weight: 400;
 }
 
 nav {
-  padding: 30px;
+  padding: 23px 62px;
+  background: #2196f3;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.24);
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
+    font-size: 14px;
+    line-height: 21px;
+    color: rgba(255, 255, 255, 0.7);
+    letter-spacing: 0.25px;
+    padding: 23px 25px;
+    transition: 0.3;
+    text-decoration: none;
     &.router-link-exact-active {
-      color: #42b983;
+      background: #1e88e5;
+      color: white;
+    }
+    &:hover {
+      background: rgba($color: #1e88e5, $alpha: 0.5);
+      color: white;
+      transition: 0.5;
     }
   }
 }
---
+++ src/views/FavoriteView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="favorite">
+    <h1>This is a favorite page</h1>
+  </div>
+</template>
